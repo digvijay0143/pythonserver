4d0baeac7d578ee7a2971841c286641d48a6a2d6
digvijay0143 <digvijayprajapat@gmail.com>
2022-09-30 12:36:17 +0530

    first commit

---
+++ sumno.py
@@ -0,0 +1,7 @@
+#Write a Python program to sum of three given integers. However, if two values are equal sum will be zero
+a=int(input("Entre value of A"))
+b=int(input("Entre value of B"))
+c=int(input("Entre value of C"))
+
+d=a+b+c
+print(d)
